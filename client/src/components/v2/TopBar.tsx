bf4f177a07afbe255b3e1ba2a2f175f790900b5b
Dsousa0 <danieltim.ds@gmail.com>
2026-06-18 20:49:38 -0300

    feat(cockpit): TopBar com telemetria e relógio ao vivo

--- client/src/components/v2/TopBar.tsx
+++ client/src/components/v2/TopBar.tsx
@@ -1,14 +1,26 @@
 // client/src/components/v2/TopBar.tsx
+import { useState, useEffect } from 'react';
 
 export default function TopBar() {
+  const [clock, setClock] = useState('--:--:--');
+  useEffect(() => {
+    const tick = () => setClock(new Date().toTimeString().slice(0, 8));
+    tick();
+    const id = setInterval(tick, 1000);
+    return () => clearInterval(id);
+  }, []);
   return (
-    <div className="v2-topbar">
-      <div className="v2-brand">
-        <img src="/logo.png" alt="" />
-        <div className="v2-brand-text">
-          <div className="v2-brand-name">Infinity Forge</div>
-        </div>
+    <header className="ck-topbar">
+      <div className="brand">
+        <img src="/logo.png" alt="Infinity Forge" />
+        <span>INFINITY FORGE</span>
       </div>
-    </div>
+      <div className="ck-tele">
+        <span><b>◇</b> SYS.ONLINE</span>
+        <span>FORGE.v2</span>
+        <span>LAT 64°N</span>
+        <span>{clock}</span>
+      </div>
+    </header>
   );
 }
